1aa858f13eceaa1c84df369afe6af6496ec8472b
saurabh-dell <saurabhkumar52940@gmail.com>
2024-04-02 13:17:47 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import{ BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
 import Home from './pages/Home'
 import SignIn from './pages/SignIn';
@@ -6,20 +6,23 @@ import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Profile from './pages/Profile';
 import Header from './components/Header';
+import PrivateRout from './components/PrivateRout';
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
-      <Header/>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/sign-in" element={<SignIn/>}/>
-        <Route path="/sign-up" element={<SignUp/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/profile" element={<Profile/>}/>
-      </Routes>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route element={<PrivateRout />} >
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
       </BrowserRouter>
     </>
   )
